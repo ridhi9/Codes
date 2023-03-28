7ceae441a59ce1ac1b1aaabcfa50c3fc6758c3d7
Ridhi <ridhisingla527@gmail.com>
2023-03-28 23:19:08 +0530

    Create Parenthesis Checker.cpp

---
+++ Parenthesis Checker.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        stack<char> s;
+       
+       for(int i=0;i<x.length();i++){
+            if(s.empty()){
+            s.push(x[i]);}
+         
+           else if((s.top() == '(' && x[i] == ')')
+                 || (s.top() == '{' && x[i] == '}')
+                 || (s.top() == '[' && x[i] == ']'))
+          {  s.pop();}
+          else{
+              s.push(x[i]);
+          }
+       }
+            if(s.empty())
+            return true;
+            else
+            return false;
+        }
+    
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   int t;
+   string a;
+   cin>>t;
+   while(t--)
+   {
+       cin>>a;
+       Solution obj;
+       if(obj.ispar(a))
+        cout<<"balanced"<<endl;
+       else
+        cout<<"not balanced"<<endl;
+   }
+}
+// } Driver Code Ends
